607cb62f7d2ca588c77b83b0a3fb4b5abecd995a
Hugh <hcmpercival@gmail.com>
2019-02-12 20:35:26 +0000

    no message

--- back-end/src/tasks/migrate-weightings-results.ts
+++ back-end/src/tasks/migrate-weightings-results.ts
@@ -3,31 +3,33 @@ import config from 'config';
 import mongoose from 'mongoose';
 
 async function run() {
-  await mongoose.connect('mongodb://127.0.0.1:' + config.get('DB_PORT') + '/osu-br', {
-    useNewUrlParser: true,
-  });
+  await mongoose.connect(
+    'mongodb://127.0.0.1:' + config.get('DB_PORT') + '/osu-br',
+    {
+      useNewUrlParser: true,
+    },
+  );
   // Reset all user results
   let count = 0;
   let updated = 0;
-  await User.find()
-    .cursor()
-    .eachAsync(async user => {
-      let changed = false;
-      if (user.results.length) {
-        user.results = [];
-        changed = true;
-      }
-      if (!user.rating.weighted) {
-        user.rating.weighted = user.rating.mu - 3 * user.rating.sigma;
-        changed = true;
-      }
+  const allUsers = await User.find({});
+  for (const user of allUsers) {
+    let changed = false;
+    if (user.results.length) {
+      user.results = [];
+      changed = true;
+    }
+    if (!user.rating.weighted) {
+      user.rating.weighted = user.rating.mu - 3 * user.rating.sigma;
+      changed = true;
+    }
 
-      count++;
-      if (changed) {
-        updated++;
-        await user.save();
-      }
-    });
+    count++;
+    if (changed) {
+      updated++;
+      await user.save();
+    }
+  }
 
   console.log(`Updated ${updated} out of ${count} total users`);
   await mongoose.disconnect();
