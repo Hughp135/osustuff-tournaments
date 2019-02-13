a3241931adb9aef83200c64a513ebb3365241615
Hugh <hcmpercival@gmail.com>
2019-02-13 21:44:44 +0000

    add short straw achievement

--- back-end/src/achievements/update-player-achievements.ts
+++ back-end/src/achievements/update-player-achievements.ts
@@ -15,6 +15,7 @@ import { giveAchievement } from './give-achievement';
 import { sendAchievementMessages } from './send-achievement-msgs';
 import { achievementAccuracy } from './round-over/accuracy';
 import config from 'config';
+import { roundFailed } from './round-over/round-failed';
 
 const TEST_MODE = config.get('TEST_MODE');
 
@@ -30,14 +31,21 @@ export async function updatePlayerAchievements(game: IGame) {
     const player = game.players.find(p => p.osuUserId === u.osuUserId);
     return player && !!player.alive;
   });
-  const passedScores = await Score.find({
+  const allRoundScores = await Score.find({
     gameId: game._id,
-    passedRound: true,
   }).sort({
     roundId: 1,
     score: -1,
     date: 1,
   });
+  const passedScores = allRoundScores.filter(s => s.passedRound);
+  const failedScores = allRoundScores.filter(
+    s =>
+      !s.passedRound &&
+      !passedScores.some( // Does not have a passing score
+        s2 => s2.userId.toHexString() === s.userId.toHexString(),
+      ),
+  );
   const passedRoundScores = passedScores.filter(
     s => s.roundId.toHexString() === game.currentRound.toHexString(),
   );
@@ -54,6 +62,7 @@ export async function updatePlayerAchievements(game: IGame) {
             await achievementVersatile(allGameUsers, passedScores),
             await achievementSpeed(allGameUsers, passedScores),
             await achievementAccuracy(passedRoundScores, aliveUsers),
+            await roundFailed(failedScores, passedScores, allGameUsers),
           ],
         );
         break;
@@ -69,14 +78,17 @@ export async function updatePlayerAchievements(game: IGame) {
         break;
     }
 
-    const achievementsQualified: IUserAchieved[] = results.reduce((acc, curr) => {
-      acc.push(...curr);
-      return acc;
-    }, []);
+    const achievementsQualified: IUserAchieved[] = results.reduce(
+      (acc, curr) => {
+        acc.push(...curr);
+        return acc;
+      },
+      [],
+    );
 
     const achievementsAwarded: IUserAchieved[] = [];
     for (const { user, achievement } of achievementsQualified) {
-      achievementsAwarded.push(...await giveAchievement(user, achievement));
+      achievementsAwarded.push(...(await giveAchievement(user, achievement)));
     }
 
     await sendAchievementMessages(achievementsAwarded, game);
--- back-end/src/game/create-game.ts
+++ back-end/src/game/create-game.ts
@@ -58,7 +58,7 @@ export async function createGame(
 
   if (TEST_MODE && game.status !== 'scheduled') {
     console.log('Creating game with sample players');
-    await addSamplePlayers(game, testPlayers || 5);
+    await addSamplePlayers(game, testPlayers || 25);
   }
 
   return game;
