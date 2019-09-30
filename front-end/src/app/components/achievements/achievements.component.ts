13a589693687dc6c09c86fc9d2ebf5ec3683100e
jvyden420 <jvyden420@gmail.com>
2019-09-29 23:54:16 -0400

    Make the page capable of displaying achievements

--- front-end/src/app/components/achievements/achievements.component.ts
+++ front-end/src/app/components/achievements/achievements.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IAchievement {
+  title: string;
+  description: string;
+  icon: string;
+}
+
 @Component({
   selector: 'app-achievements',
   templateUrl: './achievements.component.html',
   styleUrls: ['./achievements.component.scss']
 })
 export class AchievementsComponent implements OnInit {
+  public achievements: IAchievement[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.achievements = [
+      {
+        title: "a",
+        description: "b",
+        icon: "blue circle",
+      },
+      {
+        title: "these aren't real",
+        description: "i couldn't figure out how to pull all the achievements :(",
+        icon: "red checkered flag",
+      },
+      {
+        title: "shoutouts to simpleflips",
+        description: "aeaeaeaeaeae",
+        icon: "white github",
+      },
+    ]
   }
+  ngOnInit() {}
 
 }
