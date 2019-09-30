125e7b520f173090b1e1888eb30e6a6e9b2b3a87
jvyden420 <jvyden420@gmail.com>
2019-09-30 18:18:26 -0400

    Add link to show user's achievements

--- front-end/src/app/components/achievements/achievements.component.ts
+++ front-end/src/app/components/achievements/achievements.component.ts
@@ -1,11 +1,8 @@
 import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
-
-export interface IAchievement {
-  title: string;
-  description: string;
-  icon: string;
-}
+import { SettingsService } from 'src/app/services/settings.service';
+import { IUser } from '../user-profile/user-profile.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-achievements',
@@ -14,10 +11,18 @@ export interface IAchievement {
 })
 export class AchievementsComponent implements OnInit {
   public achievements: object;
+  private currentUser: IUser;
+  private subscriptions: Subscription[] = [];
 
-  constructor(private apiService: ApiService) {
+  constructor(
+    private apiService: ApiService,
+    settingsService: SettingsService,
+  ) {
     this.run(); // work around for not being able to use async in constructor
                 // most likely not the best practice, TODO: fix?
+    this.subscriptions = [
+      settingsService.user.subscribe(u => (this.currentUser = u))
+    ]
   }
   ngOnInit() {}
 
@@ -28,5 +33,9 @@ export class AchievementsComponent implements OnInit {
     } catch (e) {
       console.error(e);
     }
+  }
+
+  get loggedIn() {
+    return this.currentUser && this.currentUser.username;
   }
 }
